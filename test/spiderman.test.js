c63ec0fac0ad37acd6158800115e1b1d85ad9843
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-21 22:31:07 -0500

    Creacion de prueba con output deseado

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,15 @@
-describe("Test Suite Dummy Description", () => {
-  test('Case 1 Dummy', () => {
-    const resultOfSomething = 1 + 2
-    expect(resultOfSomething).toBe(18);
+describe('Unit Tests for Spiderman class', () => {
+  test('1) Create an spiderman object', () => {
+
+    // Escribimos codigo a utilizar tal cual
+    // Quisieramos hacer un objeto de spiderman con la informacion
+    const tomHolland = new Spiderman('Spiderman Marvel', 23, 'Tom Holland', 5, 'Disney')
+
+    // Validacion que el codigo funcione de la manera esperada
+    expect(tomHolland.name).toBe('Spiderman Marvel')
+    expect(tomHolland.age).toBe(23)
+    expect(tomHolland.actor).toBe('Tom Holland')
+    expect(tomHolland.movies).toBe(5)
+    expect(tomHolland.studio).toBe('Disney')
   });
 })
